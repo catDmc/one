bd0c73b897057e3b74d335ffeb5698d9a6083b11
xiangtao <1831641619@qq.com>
2020-05-13 12:35:11 +0800

    add

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img style="width:100%" alt="Vue logo" src="./assets/wallhaven.jpg">
+    <HelloWorld msg="asdkfjsdfasjdfasl;dj"/>
   </div>
 </template>
 
@@ -23,6 +23,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.bg {
+  width: 100%;
+  /* height: 500px */
 }
 </style>
